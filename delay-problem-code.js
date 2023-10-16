1172f0a58b4593976721545a14055960db013ef9
saif-oclouds <saif.ullah@ocloudsolutions.net>
2023-10-16 19:12:42 +0500

    updated solution delay problem

--- delay-problem-code.js
+++ delay-problem-code.js
@@ -1,32 +1,33 @@
 // Helper function for delay purpose
 function delayFunction(time) {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(), time);
-    });
+  return new Promise(resolve => {
+      setTimeout(() => resolve(), time);
+  });
 }
 
 async function delayedPromiseChain(promiseFunctions, delay) {
-    let response;
-    for (const promise of promiseFunctions) {
-        response = promise();
-        await delayFunction(delay);
-    }
+  let response;
+  for (const promise of promiseFunctions) {
+      response = await promise();
+      console.log('Promise')
+      await delayFunction(delay);
+  }
 
-    return response;
+  return response;
 }
 
-const promise1 = () => new Promise(resolve => setTimeout(() => resolve(1), 1000));
+const promise1 = () => new Promise(resolve => setTimeout(() => resolve(1), 20000));
 const promise2 = () => new Promise(resolve => setTimeout(() => resolve(2), 500));
-const promise3 = () => new Promise(resolve => setTimeout(() => resolve(3), 800));
+const promise3 = () => new Promise(resolve => setTimeout(() => resolve(3), 8000));
 const promise4 = () => new Promise(resolve => setTimeout(() => resolve(4), 300));
 
 const promiseFunctions = [promise1, promise2, promise3, promise4];
-const delay = 1000;
+const delay = 3000;
 
 delayedPromiseChain(promiseFunctions, delay)
-  .then(result => {
-    console.log("All promises have completed and result is: ", result);
-  })
-  .catch(error => {
-    console.error("An error occurred:", error);
-  });
+.then(result => {
+  console.log("All promises have completed and result is: ", result);
+})
+.catch(error => {
+  console.error("An error occurred:", error);
+});
